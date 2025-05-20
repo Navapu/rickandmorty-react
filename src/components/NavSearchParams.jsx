5f61e6ffb613110fe92091b391b6ca1069ad3c4a
andreamiguel <100772082+Navapu@users.noreply.github.com>
2025-05-20 20:14:56 +0200

    feat: create placeholder pages and navigation via URLSearchParams

---
+++ src/components/NavSearchParams.jsx
@@ -0,0 +1,12 @@
+export const NavSearchParams = () => {
+    return ( 
+        <>
+            <ul>
+                <li><a href="?page=characters">Characters</a></li>
+                <li><a href="?page=locations">Locations</a></li>
+                <li><a href="?page=episodes">Episodes</a></li>
+            </ul>
+        
+        </>
+     );
+}
