f38e17721e6382ba7e4ac77edacdcb9a2483caab
Alejandro Navarro <drojannp2@gmail.com>
2025-05-21 11:00:11 +0200

    Implement character fetching with pagination and loading/error states

--- src/components/CharactersItem.jsx
+++ src/components/CharactersItem.jsx
@@ -1,10 +1,10 @@
-const CharactersItem = ({ name, image, status }) => {
+const CharactersItem = ({ name, image, status, species}) => {
     return (
         <div className="characters-item">
             <img src={image} alt={name} />
             <div>
                 <h3>{name}</h3>
-                <p>{status}</p>
+                <p>{status} - {species}</p>
             </div>
         </div>
     );
--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,12 +1,57 @@
 import charactersData from '../mocks/mocksCharacters.json'
 import CharactersItem from '../components/CharactersItem';
+import { useEffect, useState } from 'react';
 const Characters = () => {
+    const [characters, setCharacters] = useState([])
+    const [page, setPage] = useState(1)
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const [info, setInfo] = useState([])
+    useEffect(() => {
+        fetchCharacters()
+    }, [page])
+
+    const fetchCharacters = async () => {
+        try{
+            setIsLoading(true)
+            // Cleaning up a possible previous error
+            setError(null)
+            const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
+
+            //Verify the answer
+            if(!response.ok){
+                throw new Error(`Error: ${response.status} - ${response.statusText}`)
+            }
+            const data = await response.json()
+            setCharacters(data.results)
+            setInfo(data.info)
+        }catch(err){
+            setError(err.message)
+            console.log("Error: ", {e})
+        }finally{
+            setIsLoading(false)
+        }
+    }
+
+    const prevPage = () => {
+        if(page - 1 < 1){return};
+        setPage(prev => prev - 1)
+    }
+    const nextPage = () => {
+        if(page + 1 > info.pages){return;}
+        setPage(prev => prev + 1)
+    }
     return (
         <div>
             <h1>Characters</h1>
+            <button onClick={prevPage} disabled={page === 1}>Previous</button>
+            <span>{page}</span>
+            <button onClick={nextPage} disabled={page + 1 > info.pages}>Next</button>
+            {isLoading && (<div>Loading data...</div>)}
+            {error && <p>Error loading characters: {error}</p>}
             <ul className='characters-list'>
-                {charactersData.results.map((character) => (
-                    <CharactersItem {...character} />
+                {characters.map((character) => (
+                    <CharactersItem {...character} key={character.id}/>
                 ))}
             </ul>
         </div>
