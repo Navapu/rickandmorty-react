11483e25646cd6055b905d16f5997a94e692fa65
andreamiguel <100772082+Navapu@users.noreply.github.com>
2025-05-21 20:19:53 +0200

    style: added visual point to know the status of the characters

--- src/components/CharactersItem.jsx
+++ src/components/CharactersItem.jsx
@@ -1,10 +1,15 @@
-const CharactersItem = ({ name, image, status, species}) => {
+import { GrStatusGoodSmall } from "react-icons/gr";
+
+const CharactersItem = ({ name, image, status, species }) => {
     return (
         <div className="characters-item">
             <img src={image} alt={name} />
-            <div>
+            <div className="character-information">
                 <h3>{name}</h3>
-                <p>{status} - {species}</p>
+                <div className="status">
+                    <span className={status}><GrStatusGoodSmall /></span>
+                    <p>{status} - {species}</p>
+                </div>
             </div>
         </div>
     );
